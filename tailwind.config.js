0c79598d5c8ac773cd59facf9702d79dae472315
kennice <kennice123wong@gmail.com>
2022-09-12 04:24:24 +0800

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      background: "#121212",
+      primary: "#FFFFFF",
+      secondary: "#9E9E9E",
+      active: "#CC2B5E",
+    },
+  },
+
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
